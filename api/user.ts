6bc67d9e5e41ac02dd20bc8a627458d6c3d8419b
hoodie <popping2606@gmail.com>
2022-11-26 11:15:05 +0900

    env.production.local 추가

--- api/user.ts
+++ api/user.ts
@@ -1,9 +1,6 @@
 import Axios from 'axios'
 
-export const BE_URL =
-  process.env.NODE_ENV === 'development'
-    ? `http://localhost:${process.env.NEXT_PUBLIC_LOCAL_BE_PORT || 4001}`
-    : process.env.NEXT_PUBLIC_BE_URL
+export const BE_URL = process.env.NEXT_PUBLIC_BE_URL
 
 export const axiosWithCredentials = Axios.create({
   baseURL: BE_URL + '/memo/user',
