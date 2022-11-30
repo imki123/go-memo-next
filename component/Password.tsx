1bc301bd26db2691daaffb286738982fd15b4184
hoodie <popping2606@gmail.com>
2022-11-30 20:19:05 +0900

    ts-eslint 적용

---
+++ component/Password.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+/**
+ * Password 컴포넌트
+ * @params
+ */
+const Password = () => {
+  return <StyledPassword>Password</StyledPassword>
+}
+
+export default Password
+
+const StyledPassword = styled.div``
