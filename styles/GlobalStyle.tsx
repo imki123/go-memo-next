c967c914a7da2c5b6f99acaca8933a24c8682400
hoodie <popping2606@gmail.com>
2022-12-28 21:47:27 +0900

    폰트 설정

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -34,10 +34,21 @@ export const mediaBiggerThan = (
   `
 }
 
+// 폰트 설정
+const setFont = () => {
+  return `
+    @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
+    *{
+      font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    }
+  `
+}
+
 export default function GlobalStyle() {
   return (
     <Global
       styles={css`
+        ${setFont()}
         * {
           box-sizing: border-box;
           ::-webkit-scrollbar {
