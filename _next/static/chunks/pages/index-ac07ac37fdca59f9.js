9d36dc2c51a0473ab4e307e020696fb0c23ab1c2
Im Geeyoung <popping2606@gmail.com>
2026-02-25 23:24:07 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/chunks/pages/index-ac07ac37fdca59f9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{9090:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(5426)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=9090)}),_N_E=n.O()}]);
